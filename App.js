999eb36b4393949759f4ed33c15f302d9b19fcd2
vieiraBeh <brendanascimentovieirah@gmail.com>
2024-04-10 21:30:58 -0400

    commit

--- App.js
+++ App.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [esporte, setEsporte] = useState('');
+  const [time1, setTime1] = useState('');
+  const [time2, setTime2] = useState('');
+  const [time1Pontos, setTime1Pontos] = useState('');
+  const [Time2Pontos, setTime2Pontos] = useState('');
+  const [selectTime, setSelectTime] = useState(null);
+  const [Jogadores, setJogadores] = useState([]);
+  const [nomeJogador, setNomeJogador] = useState('');
+  const [numeroJogador, setNumeroJogador] = useState('');
+
+  
+
+
+
+
+
+
 }
 
 const styles = StyleSheet.create({
